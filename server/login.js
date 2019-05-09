9245cdaf59c42433e95409d6e5ae1cb48cb98635
FlorenciaDG <48097308+FlorLuna19@users.noreply.github.com>
2019-05-09 02:29:59 -0300

    Add files via upload

--- server/login.js
+++ server/login.js
@@ -8,3 +8,19 @@ function validarUsuario(user, password){
 }
 
 module.exports.validarUsuario = validarUsuario;
+
+//////
+
+function validarUsuario(user, password){
+    if (user == "user" && password == "123"){
+        return true;
+
+    }if (user == "user2" && password =="456"){
+        return true;
+    }
+    else {
+        return false
+    }
+}
+
+module.exports.validarUsuario = validarUsuario;
--- server/server.js
+++ server/server.js
@@ -2,22 +2,36 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require("path");
+const fs = require('fs');
+const expressSession = require('express-session');
 
+
+//const mongodb = require('mongodb');
+//const MongoClient = mongodb.MongoClient;
+//const mongoURL = 'mongodb://localhost: 27017;
+//const dbName = "testdb";
+
+//Js para login
 const login = require('./login');
 
-//ya podemos usar body parser
+//Middleware de body parser para json
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-//Uso de elementos estáticos
-app.use(express.static(path.join(__dirname, '../client')))
+//Ruta para uso de elementos estáticos
+app.use(express.static(path.join(__dirname, '../client')));
 
+
+//GET
 //Ingreso al archivo index
 app.get('/', function (req, res) {
     console.log("Entre al login");
     res.sendFile(path.join(__dirname, '../client/index.html'));
 })
 
-//Le asigno una función 
+
+
+//POST / home
 app.get('/home', function (req, res) {
     console.log("Entre al home");
     res.sendFile(path.join(__dirname, '../client/home.html'));
@@ -29,10 +43,12 @@ app.get('/usuario', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/usuario.html'));
 })
 
-//Login 
+//POST / Login 
 app.post('/login', (req, res) => {
     console.log(req.body);
+
     if (req.body.user !== undefined && req.body.password !== undefined) {
+      // Si valido 'ok', redirige al home
       if (login.validarUsuario(req.body.user, req.body.password)) {
         res.send('/home');
       } else {
@@ -44,6 +60,40 @@ app.post('/login', (req, res) => {
 });
 
 
+//Uso archivo reserva
+app.get('/reserva', function (req, res) {
+  fs.readFile(path.join(__dirname, 'diasDisponibles.json'), (err, data) => {
+    if (err == undefined) {
+      let jsonDatosDiasReserva = JSON.parse(data);
+      res.send(jsonDatosDiasReserva)
+    }
+    res.render('reserva', { lugares: lugares });
+  })
+  console.log("estoy aca");
+
+})
+
+
+app.get('/reservarlugar', function (req, res) {
+
+  // Reserva guardada
+  readFile(path.join(__dirname, 'diasDisponibles.json'), (err, data) => {
+    if (err == undefined) {
+      let jsonDatosDiasReserva = JSON.parse(data);
+      res.send(jsonDatosDiasReserva)
+    }
+    render('reserva', {
+      lugares: lugares,
+      mensaje: "Se reservó ok"
+    })
+  })
+  
+  
+})
+
+
+
+//Server iniciado en puerto 4545
 app.listen(4545, function(){
     console.log("Escuchando puerto 4545");
 })
